fc53be85ed87dfa37696610e2d86cf942e386de0
Владимир Перетятько <djbob2000@gmail.com>
2023-02-05 12:17:49 +0200

    home and cast done3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import Layout from './Layout/Layout';
-import Movies from '../pages/Movies/Movies';
+// import Movies from '../pages/Movies/Movies';
 import HomePage from '../pages/HomePage/HomePage';
 import MovieDetailsPage from '../pages/MovieDetailsPage/MovieDetailsPage';
 import CastPage from '../pages/CastPage/CastPage';
@@ -11,7 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="movies" element={<Movies />} />
+        {/* <Route path="movies" element={<Movies />} /> */}
 
         <Route path="movies/:movieId" element={<MovieDetailsPage />}>
           <Route path="cast" element={<CastPage />} />
